6425c914d5af86abaf208feec53160934f066a91
Jed Smit <jedsmit1@gmail.com>
2020-02-21 10:47:27 -0800

    Updates

---
+++ precache-manifest.bbcbf71b4b78f3b498b9c003b23257df.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec28be097f9bbffbb854ca8d072a10",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "44f7228dea191b65c0e0",
+    "url": "/clicky-game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "63249120bf1ab3b7b645",
+    "url": "/clicky-game/static/css/main.5816a7a9.chunk.css"
+  },
+  {
+    "revision": "44f7228dea191b65c0e0",
+    "url": "/clicky-game/static/js/2.18fef60e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.18fef60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63249120bf1ab3b7b645",
+    "url": "/clicky-game/static/js/main.7b0187c4.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  },
+  {
+    "revision": "fe94ca62900d391830689d7a94b4223a",
+    "url": "/clicky-game/static/media/Goat_1.fe94ca62.jpg"
+  },
+  {
+    "revision": "318e6f66ea73dbd22242e0c07c4e8eca",
+    "url": "/clicky-game/static/media/Goat_10.318e6f66.jpg"
+  },
+  {
+    "revision": "069468d33ff0306289342c31c81abce6",
+    "url": "/clicky-game/static/media/Goat_11.069468d3.jpg"
+  },
+  {
+    "revision": "136c8d29812fbfd1ba22eeb64481afe2",
+    "url": "/clicky-game/static/media/Goat_12.136c8d29.jpg"
+  },
+  {
+    "revision": "13980ba681f2702918bbb0bb13c89f30",
+    "url": "/clicky-game/static/media/Goat_2.13980ba6.jpg"
+  },
+  {
+    "revision": "15cbaf19c6f5c638d0edc5e2467bf807",
+    "url": "/clicky-game/static/media/Goat_3.15cbaf19.jpg"
+  },
+  {
+    "revision": "216823c5b658adb782820ee3ea18d31f",
+    "url": "/clicky-game/static/media/Goat_4.216823c5.jpg"
+  },
+  {
+    "revision": "5993f2a3010bb92914766fec46e85062",
+    "url": "/clicky-game/static/media/Goat_5.5993f2a3.jpg"
+  },
+  {
+    "revision": "98e1a03df8ca048568743c91675da997",
+    "url": "/clicky-game/static/media/Goat_6.98e1a03d.jpg"
+  },
+  {
+    "revision": "62068714169afde2d413929843c98858",
+    "url": "/clicky-game/static/media/Goat_7.62068714.jpg"
+  },
+  {
+    "revision": "84f5656da4c1660d3c0e395bdae1fe3d",
+    "url": "/clicky-game/static/media/Goat_8.84f5656d.jpg"
+  },
+  {
+    "revision": "0b70d6db3eb931bc8c55e1f617eae210",
+    "url": "/clicky-game/static/media/Goat_9.0b70d6db.jpg"
+  },
+  {
+    "revision": "d479264d54df18d94ef0690edf82d4ba",
+    "url": "/clicky-game/static/media/grass.d479264d.jpeg"
+  }
+]);
